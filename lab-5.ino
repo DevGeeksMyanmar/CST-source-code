b636d10ccd8699a7cd35e050dc5d1c956605d531
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-01-25 08:40:19 +0630

    lab-5 source code

---
+++ lab-5.ino
@@ -0,0 +1,21 @@
+// int timer = 100;
+// void setup(){
+// for (int thisPin = 8; thisPin <= 13; thisPin++){
+// pinMode(thisPin, OUTPUT);
+// }
+// }
+// void loop(){
+// // loop from the lowest pin to the highest:
+// for (int thisPin = 8; thisPin <= 13; thisPin++){
+// // turn the pin on:
+// digitalWrite(thisPin, HIGH);
+// delay(timer);
+// digitalWrite(thisPin, LOW); 
+// }
+// // for (int thisPin = 13; thisPin >= 8; thisPin--){
+// // // turn the pin on:
+// // digitalWrite(thisPin, HIGH);
+// // delay(timer);
+// // digitalWrite(thisPin, LOW);
+// // }
+// }
